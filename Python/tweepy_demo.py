a1c0fe741e147004b77fd1b211d4262418f73bb5
DESKTOP-AM4-JF <joseph_foley@hotmail.com>
2022-03-27 17:31:44 +0100

    Got tweet url. can embed it into my own tweet

--- Python/tweepy_demo.py
+++ Python/tweepy_demo.py
@@ -10,13 +10,11 @@ import yaml
 with open(r'C:\Users\JF\Desktop\git_projects\historical-figure-questions\Docs\twitter_creds.yml', 'r') as file:
     twitter_creds =  yaml.safe_load(file)
 
-
 # Variables that contains the credentials to access Twitter API
 ACCESS_TOKEN = twitter_creds['ACCESS_TOKEN']
 ACCESS_SECRET = twitter_creds['ACCESS_SECRET']
 CONSUMER_KEY = twitter_creds['CONSUMER_KEY']
 CONSUMER_SECRET = twitter_creds['CONSUMER_SECRET']
-
 
 # Setup access to API
 def connect_to_twitter_OAuth():
@@ -30,13 +28,18 @@ def connect_to_twitter_OAuth():
 # Create API object
 api = connect_to_twitter_OAuth()
 
-
 # tweets from my stream
 public_tweets = api.home_timeline()
 tweet_text = []
+links = []
 for tweet in public_tweets:
     print('\n-----', tweet.text)
     tweet_text.append(tweet.text)
+    
+    #form url
+    link = f'https://twitter.com/{tweet.author.screen_name}/status/{tweet.id_str}'
+    print(link)
+    links.append(link)
     
 #replying to a tweet
 #https://docs.tweepy.org/en/stable/api.html#post-retrieve-and-engage-with-tweets
@@ -46,4 +49,8 @@ api.update_status(status='@CryptoWhale Dats Rite!',
                   in_reply_to_status_id='1506711629618159616')
 
 
-import yaml
+#show tweet and reply (actual thing should include their @ handle)
+url = r'https://twitter.com/CryptoWhale/status/1506711629618159616'
+api.update_status(status='Correct!',
+                  attachment_url=url)
+
