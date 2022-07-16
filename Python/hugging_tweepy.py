d6087475fc2747e4e4953086d405a2ccd8a15efa
DESKTOP-AM4-JF <joseph_foley@hotmail.com>
2022-07-16 13:18:50 +0100

    first 2 load functions

--- Python/hugging_tweepy.py
+++ Python/hugging_tweepy.py
@@ -25,6 +25,52 @@ MODEL = "deepset/roberta-base-squad2"
 # FUNCTIONS
 # =============================================================================
 # Setup access to API
+def loadRecords(records_path='../Docs/reply_records.csv',\
+                backup_path='../Docs/reply_records_BACKUP.csv'):
+    '''
+    load records of previously recorded tweets so that we dont respond to 
+    tweets where we already did so
+    '''
+    #load records
+    records = pd.read_csv(records_path)
+    
+    #save a backup (as records will later be overwritten)
+    records.to_csv(backup_path)
+    
+    return records
+
+def loadModelObjects(source=MODEL, context_path='../Docs/Marcus_Aurelius.txt'):
+    '''
+    Gets model from huggingface (first time run of this function will require
+    a download >1Gb.) Load context for the model to draw from.
+
+    Parameters
+    ----------
+    source : str, optional
+        The default is MODEL. Question Answering NLP model to pull
+        from huggingface.
+    context_path : str, optional
+        The default is '../Docs/Marcus_Aurelius.txt'. path to the 
+        context for model to draw from.
+
+    Returns
+    -------
+    qa_model : model object
+        model object
+    context : str
+        text for model to draw from
+    '''
+    #load model
+    qa_model = pipeline('question-answering', model=source, tokenizer=source)
+    
+    #load the context document
+    with open(context_path) as f:
+        context = f.read()
+    
+    context = context.replace('\n', ' ')
+    
+    return qa_model, context
+    
 def connect_to_twitter_OAuth(CONSUMER_KEY, CONSUMER_SECRET,\
                              ACCESS_TOKEN, ACCESS_SECRET):
     
@@ -34,79 +80,75 @@ def connect_to_twitter_OAuth(CONSUMER_KEY, CONSUMER_SECRET,\
     api = tweepy.API(auth)
     return api
 
+
+
 # =============================================================================
 # EXECUTE
 # =============================================================================
-#load records
-records = pd.read_csv('../Docs/reply_records.csv')
-records.to_csv('../Docs/reply_records_BACKUP.csv')
-
-#load model
-nlp = pipeline('question-answering', model=MODEL, tokenizer=MODEL)
-
-#load the context document
-with open('../Docs/Marcus_Aurelius.txt') as f:
-    context = f.read()
-
-context = context.replace('\n', ' ')
-
-#load API creds
-with open('../Docs/twitter_creds_MA.yml', 'r') as file:
-    twitter_creds =  yaml.safe_load(file)
+if __name__ =='__main__':
+    #load recorded tweets
+    records = loadRecords()
     
-##Variables that contains the credentials to access Twitter API
-ACCESS_TOKEN = twitter_creds['ACCESS_TOKEN']
-ACCESS_SECRET = twitter_creds['ACCESS_SECRET']
-CONSUMER_KEY = twitter_creds['CONSUMER_KEY']
-CONSUMER_SECRET = twitter_creds['CONSUMER_SECRET']
-
-#Create API object
-api = connect_to_twitter_OAuth(CONSUMER_KEY, CONSUMER_SECRET,\
-                               ACCESS_TOKEN, ACCESS_SECRET)
-
-#Get tweets from mentions
-public_tweets = api.mentions_timeline()
-tweet_texts = []
-links = []
-for tweet in public_tweets:
-    print('\n-----', tweet.text)
-    tweet_texts.append(tweet.text)
+    #load model and context
+    qa_model, context = loadModelObjects()
     
-    #form url
-    link = f'https://twitter.com/{tweet.author.screen_name}/status/{tweet.id_str}'
-    print(link)
-    links.append(link)
-
-new_records = []   
-#For each tweet, generate a response and reply to them
-for tweet in public_tweets:
-    #get question and link
-    question = tweet.text.strip('@AureliusRespon1 ')
-    link = f'https://twitter.com/{tweet.author.screen_name}/status/{tweet.id_str}'
-    
-    #do not respond if already done so
-    if link in records['Link'].values:
-        print(f'\nAlready Answered: {question}')
-        continue
+    #load API creds
+    with open('../Docs/twitter_creds_MA.yml', 'r') as file:
+        twitter_creds =  yaml.safe_load(file)
         
-    else:
-        print(f'Question: {question}')
+    ##Variables that contains the credentials to access Twitter API
+    ACCESS_TOKEN = twitter_creds['ACCESS_TOKEN']
+    ACCESS_SECRET = twitter_creds['ACCESS_SECRET']
+    CONSUMER_KEY = twitter_creds['CONSUMER_KEY']
+    CONSUMER_SECRET = twitter_creds['CONSUMER_SECRET']
     
-    #generate a response
-    QA_input = {'question': question, 'context': context}
-    response = nlp(QA_input)
-    print(f'Response generated: {response["answer"]}')
+    #Create API object
+    api = connect_to_twitter_OAuth(CONSUMER_KEY, CONSUMER_SECRET,\
+                                   ACCESS_TOKEN, ACCESS_SECRET)
     
-    #reply
-    tweet_reply = api.update_status(status=f'@{tweet.author.screen_name} : ' +\
-                                           response['answer'],\
-                                    attachment_url=link)
+    #Get tweets from mentions
+    public_tweets = api.mentions_timeline()
+    tweet_texts = []
+    links = []
+    for tweet in public_tweets:
+        print('\n-----', tweet.text)
+        tweet_texts.append(tweet.text)
         
-    #log that this was replied to
-    new_records.append([tweet.author.screen_name, question, response['answer'], link])
+        #form url
+        link = f'https://twitter.com/{tweet.author.screen_name}/status/{tweet.id_str}'
+        print(link)
+        links.append(link)
     
-#save log
-records = pd.concat([records,
-                     pd.DataFrame(new_records, columns=records.columns)])
-
-records.to_csv('../Docs/reply_records.csv', index=False)
+    new_records = []   
+    #For each tweet, generate a response and reply to them
+    for tweet in public_tweets:
+        #get question and link
+        question = tweet.text.strip('@AureliusRespon1 ')
+        link = f'https://twitter.com/{tweet.author.screen_name}/status/{tweet.id_str}'
+        
+        #do not respond if already done so
+        if link in records['Link'].values:
+            print(f'\nAlready Answered: {question}')
+            continue
+            
+        else:
+            print(f'Question: {question}')
+        
+        #generate a response
+        QA_input = {'question': question, 'context': context}
+        response = qa_model(QA_input)
+        print(f'Response generated: {response["answer"]}')
+        
+        #reply
+        tweet_reply = api.update_status(status=f'@{tweet.author.screen_name} : ' +\
+                                               response['answer'],\
+                                        attachment_url=link)
+            
+        #log that this was replied to
+        new_records.append([tweet.author.screen_name, question, response['answer'], link])
+        
+    #save log
+    records = pd.concat([records,
+                         pd.DataFrame(new_records, columns=records.columns)])
+    
+    records.to_csv('../Docs/reply_records.csv', index=False)
